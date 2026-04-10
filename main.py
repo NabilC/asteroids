50c8a68012cc7e5f96b02f9314ae72a716526578
Isteaque Chowdhury <ichowd4@wgu.edu>
2026-04-10 17:36:25 -0400

    Fixed window closing issue and game loop mechanics

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass
+
         screen.fill("black")
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
