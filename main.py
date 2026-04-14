500236a99e886e79aa4f84a3e9b15a62a54e9c05
Isteaque Chowdhury <ichowd4@wgu.edu>
2026-04-14 11:29:49 -0400

    Added updatable, drawable groups to manage objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -11,6 +12,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # create two empty groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # add player class to updatable and drawable before player object instance is created
+    Player.containers = (updatable, drawable)
+
+    # create the player - it gets auto added to both groups
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
 
@@ -19,7 +30,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         dt = clock.tick(60) / 1000
+
+        # update game loop to use new groups instead of player object directly
+        updatable.update(dt)
+
+        # loop over all "drawables" and .draw() them individually
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
