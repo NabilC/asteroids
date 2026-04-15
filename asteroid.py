d5d207f6fdf1f0429d35352049340e7d761ba998
Isteaque Chowdhury <ichowd4@wgu.edu>
2026-04-15 10:25:57 -0400

    Finishing touches to split asteroids properly

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import LINE_WIDTH
+import random
+from logger import log_event
+from constants import *
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -11,3 +13,30 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        # If the radius of the asteroid is less than or equal to ASTEROID_MIN_RADIUS,
+        # just return; this was a small asteroid and we're done.
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.veloctiy = b * 1.2
+
+
+# Alt. method for implementing splitting asteroid
+        # random_angle = random.uniform(20,50) # nosec
+        # new_asteroid1 = Asteroid(self.position[0], self.position[1], self.radius / 2)
+        # new_asteroid2 = Asteroid(self.position[0], self.position[1], self.radius / 2)
+        # new_asteroid1.velocity = pygame.math.Vector2.rotate(self.velocity, random_angle)*1.2
+        # new_asteroid2.velocity = pygame.math.Vector2.rotate(self.velocity, -random_angle)*1.2
